c3a9ac63511d31d9842417374d399de43ee221d0
kilic-burak <burak-kilicc@outlook.com.tr>
2024-06-25 22:03:52 +0300

    Odev 9:

--- src/main/java/odev9/main.java
+++ src/main/java/odev9/main.java
@@ -33,8 +33,8 @@ public class main {
                 continue;
             }
             catch (InputMismatchException e){
-                System.out.println("Hata! Metinsel ifade yaklandı. Sayı giriniz.");
-                sc.next();
+                throw new StringIfadeHata("Metinsel ifade ");
+
             }
             catch (Exception e){
                 System.out.println("Bir hata ile karşılaşıldı. Program sonlandırıldı.");
@@ -44,10 +44,7 @@ public class main {
                 System.out.println("Program sonlandırıldı.");
             }
         }
-
-
     }
-
 }
 
 
